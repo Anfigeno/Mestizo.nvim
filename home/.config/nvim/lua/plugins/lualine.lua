7e30dda4cba876f48384b99da8997122909db348
anfigeno <dolorcriticodevastador@proton.me>
2025-07-05 13:13:02 -0500

    [Corrección] Quitamos la integración que ya no existe

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -2,15 +2,11 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		local mestizo_lualine = require("mestizo.claves.integraciones.especial.lualine")
-
 		require("lualine").setup({
 			options = {
-				theme = mestizo_lualine.tema,
 				component_separators = "",
 				section_separators = { left = " ", right = " " },
 			},
-			sections = mestizo_lualine.secciones,
 		})
 	end,
 }
